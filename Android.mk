07c791454e006fe79824055eb3eb6c195f4567f5
AnierinB <anierin@evolution-x.org>
2026-07-01 08:37:41 +0000

    odin2: Import blobs & firmware from Odin2.20240131.094622
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ Android.mk
@@ -0,0 +1,31 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),odin2)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,20f8ec93bdff7228a77ef65d4299a88ecbfd6707)
+$(call add-radio-file-sha1-checked,radio/aop.img,823ae2cafb18e28ad4b0da228ff1136de4666371)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,df06584c934e327597be30a128589c9559525602)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,56b0a338ce7f8df25a9e58e77a5fac914b001fc9)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,c6256e69b2dfb1114c1d82f2e29c222bc710d02d)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,f094e7209efe78a1c8d9074f64f6bc998e3037aa)
+$(call add-radio-file-sha1-checked,radio/dsp.img,5b42cbe347425fede1942ba83da0e4b552d28a18)
+$(call add-radio-file-sha1-checked,radio/hyp.img,4d9ced0265e91ef668d6b056d0878a9350c13b1b)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,42ebf4e69a5d904009a9bc78fe01c1d96e9d7d0f)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,2dee8c9cce09b8687a8b64ea135ce97b644c9b54)
+$(call add-radio-file-sha1-checked,radio/modem.img,6813e4c8fb5d60dad13d580e9cd11dc030d897ef)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,5749ef3f1551674a43698cfda3eeb216d0305d7b)
+$(call add-radio-file-sha1-checked,radio/multiimgqti.img,49b76bdc34d6ebf77c849d157d59bf56d57f6e3c)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,a1c002b1e564d3219adb262408c83c4452e4c469)
+$(call add-radio-file-sha1-checked,radio/shrm.img,f15ce824ab3c08a101d51d1f8f1fb86ba09a9e3b)
+$(call add-radio-file-sha1-checked,radio/tz.img,929fe7db78be58216b43132ab6bcfb5f94dfd94c)
+$(call add-radio-file-sha1-checked,radio/uefi.img,327328cde40e42021bed74844eaffeee4e9d3f1c)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,26819e96eb287487012e7a8f86f78c41ad7876e7)
+$(call add-radio-file-sha1-checked,radio/xbl.img,16c2f8508fbbe92597cfc98eabe86eafa7374d95)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,3cd5036c5c5920758a0f9da713125634c4b7c587)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,6552b6ff531343453a7e867d52e96ade93bd89c3)
+
+endif
